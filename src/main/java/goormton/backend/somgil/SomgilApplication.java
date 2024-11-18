d8f6f2f9ae12c28af92ffd9912b15aa1b33fed21
T-ferret <hs381717@gmail.com>
2024-11-18 14:06:38 +0900

    자바 프로젝트

---
+++ src/main/java/goormton/backend/somgil/SomgilApplication.java
@@ -0,0 +1,13 @@
+package goormton.backend.somgil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomgilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SomgilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/goormton/backend/somgil/SomgilApplicationTests.java
@@ -0,0 +1,13 @@
+package goormton.backend.somgil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomgilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
